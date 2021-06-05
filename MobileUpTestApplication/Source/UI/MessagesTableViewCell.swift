2aa16451f088ba02a3b35be52e7f1365665165a2
Azamat <iazamat.far@gmail.com>
2021-06-06 02:04:51 +0300

    base message setupping

--- MobileUpTestApplication/Source/UI/MessagesTableViewCell.swift
+++ MobileUpTestApplication/Source/UI/MessagesTableViewCell.swift
@@ -16,5 +16,48 @@ class MessagesTableViewCell: UITableViewCell {
     static let cellReuseIdentifier: String = "MessagesTableViewCell"
 
     func setup(withUserName userName: String, userAvatarURL avatarURL: String, messageText: String, receivingDate: Date) {
+        userNameLabel?.text = userName
+        messageLabel?.text = messageText
+
+        let calendar = Calendar.current.dateComponents([.year, .month, .weekOfYear, .day, .weekday, .hour, .minute, .second], from: receivingDate, to: Date())
+        switch calendar {
+        case let x where x.year ?? .zero > 1:
+            dateTimeLabel?.text = "\(x.year ?? .zero) years"
+        case let x where x.year ?? .zero == 1:
+            dateTimeLabel?.text = "one year"
+        case let x where x.month ?? .zero > .zero:
+            dateTimeLabel?.text = "\(x.month ?? .zero) months"
+        case let x where x.weekOfYear ?? .zero > 0:
+            dateTimeLabel?.text = "\(x.weekOfYear ?? .zero) weeks"
+        case let x where x.day ?? .zero > 2:
+            switch x.weekday {
+            case 1:
+                dateTimeLabel?.text = "Monday"
+            case 2:
+                dateTimeLabel?.text = "Tuesday"
+            case 3:
+                dateTimeLabel?.text = "Wednesday"
+            case 4:
+                dateTimeLabel?.text = "Thursday"
+            case 5:
+                dateTimeLabel?.text = "Friday"
+            case 6:
+                dateTimeLabel?.text = "Saturday"
+            case 7:
+                dateTimeLabel?.text = "Sunday"
+            default:
+                dateTimeLabel?.text = ""
+            }
+        case let x where x.day ?? .zero > 1:
+            dateTimeLabel?.text = "Yesterday"
+        case let x where x.hour ?? .zero > .zero:
+            dateTimeLabel?.text = "\(x.hour ?? .zero) hours"
+        case let x where x.minute ?? .zero > .zero:
+            dateTimeLabel?.text = "\(x.minute ?? .zero) minutes"
+        case let x where x.second ?? .zero > .zero:
+            dateTimeLabel?.text = "\(x.second ?? .zero) seconds"
+        default:
+            dateTimeLabel?.text = "Now"
+        }
     }
 }
